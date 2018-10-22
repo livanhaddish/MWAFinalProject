abac484fe8222213618e89fa87632bb1490e6fc6
MerhawiG <annamereco@gmail.com>
2018-10-21 19:22:10 -0500

    autorization

--- onlineApp/src/app/login/login.component.ts
+++ onlineApp/src/app/login/login.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import{ AuthService} from '../service/auth.service';
-<<<<<<< HEAD
 import { Router } from '@angular/router'
-=======
-import { identifierModuleUrl } from '@angular/compiler';
->>>>>>> 286bc838434997d9503f79bddd4e62997e881a00
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,7 +8,6 @@ import { identifierModuleUrl } from '@angular/compiler';
 })
 export class LoginComponent implements OnInit {
   loginUserData={}
-<<<<<<< HEAD
   constructor(private _auth: AuthService,private _router: Router) { }
 
   ngOnInit() {
@@ -29,23 +24,3 @@ export class LoginComponent implements OnInit {
   }
 }
 
-=======
-  constructor(private _auth: AuthService) { }
-
-  ngOnInit() {
-  }
-loginUser(){
-  this._auth.loginUser(this.loginUserData)
-  .subscribe(
-    res => console.log(res),
-    err => console.log(err)
-  )
-}
- getUsers(){
-   this._auth.getUsers().subscribe(
-     res => console.log(res),
-     err => console.log(err)
-   )
- }
-}
->>>>>>> 286bc838434997d9503f79bddd4e62997e881a00
--- onlineApp/src/app/service/auth.service.ts
+++ onlineApp/src/app/service/auth.service.ts
@@ -4,14 +4,9 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class AuthService {
-  
   private _registerUrl = "http://localhost:3090/app/register";
   private _loginUrl = "http://localhost:3090/app/login";
-  private _getUserUrl = "http://localhost:3090/exams"
-  private _getOneUserUrl = "http://localhost:3090/exams/edit"
-  constructor(private http: HttpClient) {
-    
-   }
+  constructor(private http: HttpClient) { }
 
   registerUser(user) {
     return this.http.post<any>(this._registerUrl, user)
